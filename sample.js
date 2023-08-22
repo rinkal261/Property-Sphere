3d2de2fb571a4676a68a589ca454c322a9daac61
rinkal <rinkalteli@gmail.com>
2023-08-22 09:45:55 +0530

    new commit

---
+++ sample.js
@@ -0,0 +1,18 @@
+
+// function scrolll(){
+//     var left = document.querySelector(".insight-box1")
+//     console.log(left);
+//     left.scrollBy(1070, 0);
+// }
+// function scrollr(){
+//     var right = document.querySelector(".insight-box1")
+//     right.scrollBy(1070, 0);
+// }
+
+let arrow = document.querySelectorAll(".container4-box2");
+
+arrow.forEach(btn => {
+    scroll-btn.addEventListener("click", ()=>{
+        console.log(scroll-btn.id);
+    })
+})
